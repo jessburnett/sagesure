8a42acab23c2306d4e243c7b60e7ac720bd8cc48
Jessica Burnett <jess.a.burnett@gmail.com>
2022-07-26 08:32:21 -0600

    add state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
+import { useSelector } from 'react-redux';
 
 function App() {
+        //access data in store
+        const state = useSelector((state) => state);
+        console.log(state);
   return (
     <div className="App">
       
--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { store } from './state/store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    // wrap root so all components have access to store
+    {/* wrap root so all components have access to store */}
     <Provider store={store}>
       <App />
    </Provider>
