613bc4e5a543d5098c46c7d3e3556de58ba18165
Jessica Burnett <jess.a.burnett@gmail.com>
2022-07-26 09:08:51 -0600

    add ui dep/with buttons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './state/index';
 
 function App() {
         //access data in store
-        const state = useSelector((state) => state);
-        console.log(state);
+        const account = useSelector((state) => state.account);
+        const dispatch = useDispatch();
+
+        const { depositClaim, withdrawClaim } = bindActionCreators(actionCreators, dispatch);
   return (
     <div className="App">
-      
+      <div>{account}</div>
+      <button onClick={() => depositClaim(1500)}>Claim Deposit</button>
+      <button onClick={() => withdrawClaim(1500)}>Claim Withdraw</button>
     </div>
   );
 }
